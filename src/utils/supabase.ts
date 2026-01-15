8e36eee9f297508cf543452f67cb1b63a2678bd7
Jeff <jeffmbogo004@gmail.com>
2026-01-15 12:51:39 +0300

    Supabase

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://hbmdvtbpunrbkqxlphyd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibWR2dGJwdW5yYmtxeGxwaHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5MjU5MjksImV4cCI6MjAzNTUwMTkyOX0.JvzaQ4RqBeBPYg3SPK9eFCVNn33bdaGQhWd7_A3HEQY",
+  "https://uxmffhopsgxeeejwbofp.supabase.co",
+  "sb_publishable_SnKe4r-2AZzckzoZ8EUeOQ_BPA6XpUv",
 );
 
 export default supabase;
